09bd8960f2d9f48ba488a37faf81d9ea3275238d
Crashmast3r <rickyvanvliet11@hotmail.com>
2020-12-09 22:44:05 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// object containing the arays of the components of the messages
+// and methods for randomizing the output
+const Blessings = {
+    offer: ['Sweet Roll', 'Gold Coin'],
+    divine: ['Talos', 'Mara'],
+    blessing: ['Money', 'Hope', 'Love'],
+
+    getOffer() {
+        let i = Math.floor(Math.random() * this.offer.length);
+        return this.offer[i];
+    },
+
+    getDivine() {
+        let i = Math.floor(Math.random() * this.divine.length);
+        return this.divine[i];
+    }
+
+    getBlessing() {
+        let i = Math.floor(Math.random() * this.blessing.length);
+        return this.blessing[i];
+    }
+}
